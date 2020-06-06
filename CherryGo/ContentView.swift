14da7912f2c0149b37a7c6193c87ce1530cea62f
pank <pqhitpan@gmail.com>
2020-06-06 16:16:16 -0400

    Initial Commit

---
+++ CherryGo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CherryGo
+//
+//  Created by Kevin Pan on 2020-06-06.
+//  Copyright © 2020 Kevin Pan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
